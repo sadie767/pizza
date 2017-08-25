fe77cc3f0cbc568dce83a35eb8806efef406cfe7
rm <biggbangpow767@yahoo.com>
2017-08-25 11:08:20 -0700

    add a commit with a working order form using a constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // business logic
-// function Pizza (size, toppings, number) {
-//   this.size = size;
-//   this.toppings = [];
-//   this.number = number;
-// // }
+function Pizza (size, toppings, number) {
+  this.size = size;
+  this.toppings = toppings;
+  this.number = number;
+}
 // function Toppings (pickles, sardines, lettuce) {
 //   this.pickles = pickles;
 //   this.sardine = sardines;
@@ -12,12 +12,17 @@
 
 //user-interface logic$
 $(document).ready(function() {
-  $("#size").change(function() {
-    var pizzaSize = $("#size").val();
-    alert(pizzaSize);
-  });
-  $("#toppings").change(function() {
-    var pizzaToppings = $("#toppings").val();
-    alert(pizzaToppings);
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    var chooseSize = $("#size").val();
+    var chooseToppings = $("#toppings").val();
+    var chooseNumber = $("#number").val();
+
+    var sendObject = new Pizza(chooseSize, chooseToppings, chooseNumber);
+
+  $("#showOrder").show();
+  $("#showSize").text(sendObject.size);
+  $("#showToppings").text(sendObject.toppings);
+  $("#showNumber").text(sendObject.number);
   });
 });
